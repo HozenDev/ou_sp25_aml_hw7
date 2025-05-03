1e9e97c2f235fdfc61710b6f3868e5aeb367d733
Hozen <enzo.durel@gmail.com>
2025-05-03 15:34:35 -0500

    up

--- code/plot.py
+++ code/plot.py
@@ -94,22 +94,24 @@ def load_results(results_dir):
 
 def predict_example(args, model):
     # Load validation data
-    ds = create_single_dataset(base_dir=args.dataset,
-                               full_sat=False,
-                               partition='valid',
-                               patch_size=256,
-                               fold=0,
-                               cache_path=None,
-                               repeat=True,
-                               batch_size=8,
-                               prefetch=2,
-                               num_parallel_calls=4)
+    timesteps = 10
+    beta, alpha, gamma = compute_beta_alpha2(timesteps, 0.0001, 0.02, 0, 0.1)
+
+    ds, _ = create_diffusion_dataset(
+        alpha=None,
+        base_dir=args.dataset,
+        patch_size=256,
+        fold=0,
+        filt='*[012345678]',
+        cache_dir=args.cache,
+        repeat=True,
+        batch_size=8,
+        prefetch=2,
+        num_parallel_calls=4
+    )
     
     I, L = next(iter(ds))
     print(I.numpy().shape, L.numpy().shape)
-
-    timesteps = 10
-    beta, alpha, gamma = compute_beta_alpha2(timesteps, 0.0001, 0.02, 0, 0.1)
 
     # Start from random noise
     Z = np.random.normal(loc=0, scale=1.0, size=(args.batch, 256, 256, 3)).astype(np.float32)
